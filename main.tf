90a896fd073629573b709b96eb6612893ded211d
Project <98093306+shahsuleman1000@users.noreply.github.com>
2023-02-26 01:30:44 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_ecs_cluster" "hello-world-cluster" {
+  name = "hello-world-cluster"
+}
+
 resource "aws_ecs_task_definition" "web" {
   family                   = "hello-world"
   container_definitions    = <<DEFINITION
@@ -26,6 +30,7 @@ resource "aws_ecs_service" "web" {
   name            = "hello-world-service"
   task_definition = aws_ecs_task_definition.web.arn
   desired_count   = 1
+  cluster         = aws_ecs_cluster.hello-world-cluster.arn
 
   network_configuration {
     subnets          = ["subnet-011dad032feb4a3ad"]
